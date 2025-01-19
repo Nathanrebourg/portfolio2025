32965939557afaff522d6beb5dead1cb5d9e5112
Nathan REBOURG <nathanreb69@gmail.com>
2025-01-19 16:46:48 +0100

    Update envoi_email.php

--- envoi_email.php
+++ envoi_email.php
@@ -1,19 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer l'adresse e-mail de l'utilisateur
+    // Récup email
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    // Vérifier si l'adresse e-mail est valide
+    // Validation email
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Lien vers votre portfolio sur Google Drive
         $portfolioLink = "https://drive.google.com/drive/u/0/folders/1WVnshN51DxcjQ2zfqdsz18StRyXt3XnE";
 
-        // Préparer les détails de l'e-mail
         $subject = "Votre portfolio";
         $message = "Bonjour,\n\nMerci de votre intérêt ! Vous pouvez consulter mon portfolio en cliquant sur le lien ci-dessous :\n\n$portfolioLink\n\nCordialement,\n[Votre nom]";
         $headers = "From: votre-email@example.com";
 
-        // Envoyer l'e-mail
+        // Envoi email
         if (mail($email, $subject, $message, $headers)) {
             echo "L'e-mail a été envoyé avec succès !";
         } else {
